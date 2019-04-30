71d8c70f3180b510beed18be8745d8ba6dffda3e
linjiaqin <1263457581@qq.com>
2019-04-30 21:10:55 +0800

    dockerclient

---
+++ dockerclient/src/main/java/com/ljq/DockerclientApplication.java
@@ -0,0 +1,13 @@
+package com.ljq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerclientApplication.class, args);
+	}
+
+}
